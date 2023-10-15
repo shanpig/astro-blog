7c5a9cd24f76a6e7ad3630e57f4fe80130c09bdb
david <davidLiao@hahow.in>
2023-10-15 17:45:44 +0800

    fix: add base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://shanpig.github.io',
+  base: 'astro-blog',
   integrations: [mdx(), sitemap()],
 });
